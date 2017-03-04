d43e37215986d1a8c6131511cda5da0e8884985a
Ruben Oostinga <ruben.oostinga@gmail.com>
2017-03-04 15:19:41 +0100

    Add snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,15 @@
 import React, { render } from 'preact-compat';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   render(<App />, div);
+});
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <App />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
